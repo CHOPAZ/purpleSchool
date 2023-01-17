24795849c16efb9a18006de41c0314a04983ce0e
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-17 21:27:08 +0500

    Arithmetic operators

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+// Basic arithmetic operators
 
-let namae = 'Anton';
-const age = 15;
+/* */
 
-console.log(age, namae);
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3 // 2 * 2 * 2 
+console.log(volume);
+
+// Lines оператор "+" превращается в оператор конкетинации в рамкар работы со строками
+
+const city = 'Moscow';
+const street = 'Novoslobodskaya'
+console.log(city + ', ' + street);

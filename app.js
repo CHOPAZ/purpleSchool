ac9fe8b7b1bf2022791004c21a56bf20c4dc1c01
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-17 22:53:50 +0500

    Type conversion

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const projectName = 'Website';
-const price = 2000;
-const author = 'Anton'
-
-const template = author + ' buy ' + projectName + ' price ' + price
-console.log(template);
-
-// Шаблонные строки ``
-
-const template2 = `${author} buy ${projectName} price ${price}`
-console.log(template2);
-
-const template3 = 'Project \n' + 'Price: ' + price + '$'
-console.log(template3);
-
-const template4 = `Project
-Price: ${price}$`;
-console.log(template4);
+const age = '18';
+console.log(age + 5); //185
+console.log(Number(age) + 5); //23
+console.log(age - 3); // 15
+const userName = 'Vasya'
+console.log(userName + 5); //Vasya5
+console.log(Number(userName) + 5); // NaN
+console.log(typeof NaN);
+console.log(4 + 7); // 11
+console.log(String(4) + 7); // 47
+console.log(Boolean(0)); // false
+console.log(Boolean(1)); // true
+console.log(Boolean(-1)); // true
+console.log(Boolean(2)); // true
+console.log(Boolean('')); // false
+console.log(true + 2); // 3

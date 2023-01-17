157122c970dea3fa826c33b302695db754a54781
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-17 22:59:16 +0500

    False from other types

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const age = '18';
-console.log(age + 5); //185
-console.log(Number(age) + 5); //23
-console.log(age - 3); // 15
-const userName = 'Vasya'
-console.log(userName + 5); //Vasya5
-console.log(Number(userName) + 5); // NaN
-console.log(typeof NaN);
-console.log(4 + 7); // 11
-console.log(String(4) + 7); // 47
-console.log(Boolean(0)); // false
-console.log(Boolean(1)); // true
-console.log(Boolean(-1)); // true
-console.log(Boolean(2)); // true
-console.log(Boolean('')); // false
-console.log(true + 2); // 3
+
+console.log(Boolean(0)); //false
+console.log(Boolean('')); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean(null)); //false
+console.log(Boolean(Number('sx'))); //false
+console.log(Boolean(NaN)); //false

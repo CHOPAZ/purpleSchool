74e112d46c46cf2c93c8d404cb65c1b6d51fe7ce
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-18 20:08:40 +0500

    Switch

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-const secretNumber = '7';
+const role = 'manager';
 
-if (secretNumber == 7) {
-  console.log('Угадал не строго');
+if (role === 'manager') {
+  console.log('Manager');
+} else if (role === 'admin') {
+  console.log('Admin');
+} else if (role === 'Ceo') {
+  console.log('CEO');
+} else {
+  console.log('Go back');
 }
-if (Number(secretNumber) === 7) {
-  console.log('Угадал строго');
+
+switch (role) {
+  case 'manager': // role === 'manager'
+    console.log('Manager');
+    break;
+  case 'admin': // role === 'admin'
+    console.log('Admin');
+    break;
+  case 'ceo': // role === 'ceo'
+    console.log('Ceo');
+    break
+  default:
+    console.log('Go back');
+    break;
 }
-// == (нестрогое равенство) - проверка только по значению
-//  === (строгое равенство) - проверка по значению и типу
+
+switch (role) {
+  case 'manager':
+  case 'admin':
+    console.log('Не руководитель');
+    break;
+  case 'ceo':
+    console.log('Руководитель');
+    break
+  default:
+    console.log('Go back');
+    break;
+}
+
+const num = 1;
+
+switch (true) {
+  case num > 0: // true === num > 0
+    console.log('Положительный');
+    break;
+  case num < 0: // true === num < 0
+    console.log('Отрицательный');
+    breake;
+  default: // true === num === 0
+    console.log('Ноль!');
+    break;
+}

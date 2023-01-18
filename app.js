8715903ca98afc063fba40ce3ac5cf81c2626f70
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-18 19:44:56 +0500

    Equality operators

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-/*
-	Васи положил 12 000$ на вклад 7% годовых с
-	капитализацией 1 раз в месяц.
-	Вывести в консоль, сможет ли он купить дом за 13 500$
-	через 2 года после снятия вклада. И остаток после покупки.
+const secretNumber = '7';
 
-	Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
-*/
-
-const money = 12000;
-const depositProcentMonth = 0.0058;
-const depositLength = 24
-const houseCost = 13500;
-
-const result = money * (1 + depositProcentMonth) ** depositLength
-
-
-if (result >= houseCost) {
-  console.log(`You can buy a house, and you will have ${Math.round(result - houseCost)}$`);
-} else {
-  console.log(`You won't be able to buy a house you will have ${Math.round(result)} $`);
+if (secretNumber == 7) {
+  console.log('Угадал не строго');
 }
+if (Number(secretNumber) === 7) {
+  console.log('Угадал строго');
+}
+// == (нестрогое равенство) - проверка только по значению
+//  === (строгое равенство) - проверка по значению и типу

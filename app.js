57dd7a8abf0ac283903cc079ab64245c2852aaa9
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-19 22:05:15 +0500

    Operators with other types

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+console.log('Вася' || 'Oleg'); // Вася (всегда первый оперант если он true)
+console.log(false || 'Oleg'); // Oleg
+console.log('Vasya' || false); // Vasya
+console.log(false || false); // false
+
+console.log('Vasya' && 'Oleg'); // Oleg (всегда второй операнд если первый true)
+console.log(false && 'Oleg'); // false
+console.log('Vasya' && false); // false 
+console.log(false && false); //  false
+
+let a;
+const userName = a || 'Petay';
+console.log(userName); // Petya (т.к а не задано)
+
+let b = 'Marina'
+const userName1 = b || 'Petya'
+console.log(userName1); // Marina
+
 const isAdmin = true;
-const canWrite = true;
-
-console.log(`Системный файл ${isAdmin && canWrite}`);
-console.log(`Обычный файл файл ${isAdmin || canWrite}`);
-console.log(`Инвертируем права админа ${!isAdmin}`);
-console.log(`Инвертируем права записи ${!canWrite}`);
-
-const isEdited = true;
-const isSuperAdmin = true;
-
-console.log(`Системный файл с редактированием ${isAdmin && canWrite && (!isEdited || isSuperAdmin)}`);
-
-let a = 7;
-
-if (a === -8 || a === 22) {}
+const fileName = isAdmin && 'file.mp4';
+console.log(fileName);// file.mp4

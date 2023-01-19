2319f9723bf68d32d211de6b3d88d5de7d7bde99
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-19 22:26:08 +0500

    Practice - Rights check

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-// Оператор нулевого слияния ??  Nullish coalescing operator проверяет на null и undefined
+/*
+	Пользователь хочет приобрести игру в магазине
+	Он может это сделать отлько если:
+	- Eго баланс больше 1000 (balance) 
+	или число бонусов больше 100 (bonusBalance)
+	- Он не забанен (isBanned)
+	- Игра не кулена (isExist)
+	- Игра в продаже (isSelling)
+	Напишите условие для покупки и выведите в консоль
+	результат
+*/
 
-const age = 0;
-const userName1 = '';
-console.log(age || 18); // 18
-console.log(age ?? 18); // 0
+const balans = 1000;
+const bonusBalance = 100 ;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+
+const res = (balans > 1000 || bonusBalance > 100)
+  && !isBanned 
+  && !isExist 
+  && isSelling
+
+console.log(`Ты можешь купить игру?  ${res ? 'Da' : 'Net'}`);
+

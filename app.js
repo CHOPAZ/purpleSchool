8014b7e7d0a1bf04cf90bec962d6a5bdc3fc1bbe
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-19 22:11:56 +0500

    Nullish coalescing operator ??

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-console.log('Вася' || 'Oleg'); // Вася (всегда первый оперант если он true)
-console.log(false || 'Oleg'); // Oleg
-console.log('Vasya' || false); // Vasya
-console.log(false || false); // false
+// Оператор нулевого слияния ??  Nullish coalescing operator проверяет на null и undefined
 
-console.log('Vasya' && 'Oleg'); // Oleg (всегда второй операнд если первый true)
-console.log(false && 'Oleg'); // false
-console.log('Vasya' && false); // false 
-console.log(false && false); //  false
-
-let a;
-const userName = a || 'Petay';
-console.log(userName); // Petya (т.к а не задано)
-
-let b = 'Marina'
-const userName1 = b || 'Petya'
-console.log(userName1); // Marina
-
-const isAdmin = true;
-const fileName = isAdmin && 'file.mp4';
-console.log(fileName);// file.mp4
+const age = 0;
+const userName1 = '';
+console.log(age || 18); // 18
+console.log(age ?? 18); // 0

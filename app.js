294027d2042014b9b7667573e67a6e5db206bb1d
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-26 22:13:19 +0500

    Practice - function simplification

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-// Стрелочные функции
+/* Переписать функцию в стрелочную */
 
-// Обычная функция
-function powerOfTwo(num) {
-  return num * num;
+function toPower(num, power) {
+  const res = num ** power;
+  return res;
 }
-console.log(powerOfTwo(5));
 
-// Стрелочная функция
+console.log(toPower(2, 3));
 
-const poft = (num) => num * num;
-
-const poft1 = num => num * num;
-
-const poft2 = num => {
-  console.log(num);  
-  return num * num
-};
-
-
-console.log(poft(5));
-
-// Отличие всплытие, контекст - про контекст будет дальше
-
-
-console.log(poft3(5)); // ошибка, poft еще не задана
-const poft3 = (num) => num * num;
+const toPowerArrow = (num, power) => num ** power
+console.log(toPowerArrow(2, 3));

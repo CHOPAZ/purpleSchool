be2fa76cea3ba02b0bf84b96ef5a4ac6c61f0c0d
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-31 21:45:51 +0500

    Finding an element

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-const users = ['Anya', 'Vika', 'Katya'];
-console.log(users);
+const roles = ['user', 'admin', 'manager'];
 
-//ЗАмена элемента массива
-users[2] = 'Cristina';
-console.log(users);
+// indexOf - ищет первое вхождение. Обычно используется для получения конкретного индекса
+const elIndex = roles.indexOf('admin');
+console.log(elIndex);
+const elIndex2 = roles.indexOf('superUser');
+console.log(elIndex2); // -1, т.к. нет такого элементта в массиве
 
-//Добавление элемента в массив
-users[3] = 'Nikita';
-console.log(users);
+if (roles.indexOf('admin') >= 0) {
+  console.log('Доступ есть');
+}
 
-users[5] = 'Sasha';// 4 - элемент массива юудет empty
-console.log(users);
-
-// Удобные методы добавления и удаления элементов
-
-users.push('Pasha'); // Добавление в КОНЕЦ массива
-// const arrLength = users.push('Pasha'); // длина массива 7
-
-users.unshift('Dima') // Добавление в НАЧАЛО массива
-users.pop(); // Удаление ПОСЛЕДНЕГО элемента массива
-users.shift();// Удаление ПЕРВОГО элемента массива
-
+// includes - возвращает true or false
+console.log(roles.includes('admin')); //true

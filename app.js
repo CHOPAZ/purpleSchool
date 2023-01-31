de2dfa806f0d54db95e5105aaef3d940bff1302c
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-31 23:01:27 +0500

    Practice - Update the task list

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+/*
+	Дан список задач
+	const tasks = ['Задача 1'];
+	Сделать функции:
+	- Добавление задачи в конец
+	- Удаление задачи по названию
+	- Перенос задачи в начало списка по названию 
+	Всегда меняем исходный массив
+*/
 
-const url = 'auth/user/login';
-//split - разбить, аргументом принимает символ по которому нужно разбить
-const res = url.split('/')
-console.log(res); //['auth', 'user', 'login']
+const tasks = ['Задача 1',];
 
-//join - объединяет элементы массива в строку
-console.log(roles.join('-'));// user-admin-manager-superuser
+const addNewTask = (task) => tasks.push(task);
+addNewTask('Задача 2');
+addNewTask('Задача 3');
+// console.log(tasks);
+
+
+const removeTask = (task) => {
+  const index = tasks.indexOf(task);
+  if(index === -1){
+    return
+  }
+  tasks.splice(index,1)
+};
+removeTask('Задача 2');
+// console.log(tasks);
+
+
+const priorityTask =(task) => {
+  const index = tasks.indexOf(task);
+  console.log(index);
+	if (index === -1) {
+		return;
+	}
+	const oldTask = tasks[index];
+	tasks.splice(index, 1);
+	tasks.unshift(oldTask);
+
+}
+addNewTask('Задача 4')
+priorityTask('Задача 4');
+console.log(tasks);

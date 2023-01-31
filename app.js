94d63eeb07901b80c5f4d0e6134414353b6adeb2
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-01-31 23:16:10 +0500

    Principle DRY ( Don't repeat yourself)

--- app.js
+++ app.js
@@ -6,38 +6,35 @@
 	- Удаление задачи по названию
 	- Перенос задачи в начало списка по названию 
 	Всегда меняем исходный массив
+
+  DRY - Don't repeat yourself
 */
 
 const tasks = ['Задача 1',];
 
 const addNewTask = (task) => tasks.push(task);
-addNewTask('Задача 2');
-addNewTask('Задача 3');
-// console.log(tasks);
-
 
 const removeTask = (task) => {
   const index = tasks.indexOf(task);
   if(index === -1){
     return
   }
-  tasks.splice(index,1)
+  return tasks.splice(index,1)
 };
-removeTask('Задача 2');
-// console.log(tasks);
-
 
 const priorityTask =(task) => {
-  const index = tasks.indexOf(task);
-  console.log(index);
-	if (index === -1) {
-		return;
-	}
-	const oldTask = tasks[index];
-	tasks.splice(index, 1);
-	tasks.unshift(oldTask);
+  const res = removeTask(task);
+  if (!res) {
+    return
+  }
+  tasks.unshift(res[0]);
 
 }
+addNewTask('Задача 2');
+addNewTask('Задача 3');
+console.log(tasks);
+removeTask('Задача 2')
+console.log(tasks);
 addNewTask('Задача 4')
 priorityTask('Задача 4');
 console.log(tasks);

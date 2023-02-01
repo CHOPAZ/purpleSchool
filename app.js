59b2f1e5481c16e133b9250cf8cbb5df2f321fd4
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-01 20:06:09 +0500

    Destructuring

--- app.js
+++ app.js
@@ -1,40 +1,8 @@
-/*
-	Дан список задач
-	const tasks = ['Задача 1'];
-	Сделать функции:
-	- Добавление задачи в конец
-	- Удаление задачи по названию
-	- Перенос задачи в начало списка по названию 
-	Всегда меняем исходный массив
+//Деструктуризация
+const userData = ['Anton', 25, 'Moscow']
+// const [userName, age ,city] = userData; // Anton 25 Moscow
+// const [userName, age] = userData; // Anton 25
+const [userName, _, city] = userData; // Anton Moscow
 
-  DRY - Don't repeat yourself
-*/
 
-const tasks = ['Задача 1',];
 
-const addNewTask = (task) => tasks.push(task);
-
-const removeTask = (task) => {
-  const index = tasks.indexOf(task);
-  if(index === -1){
-    return
-  }
-  return tasks.splice(index,1)
-};
-
-const priorityTask =(task) => {
-  const res = removeTask(task);
-  if (!res) {
-    return
-  }
-  tasks.unshift(res[0]);
-
-}
-addNewTask('Задача 2');
-addNewTask('Задача 3');
-console.log(tasks);
-removeTask('Задача 2')
-console.log(tasks);
-addNewTask('Задача 4')
-priorityTask('Задача 4');
-console.log(tasks);

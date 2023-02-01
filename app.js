b1b53c8fe1dde72f8543046af1e05085a6eb33f9
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-01 23:11:02 +0500

    Practice - Cycle in reverse

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-//Break and continue
+/*
+	Задача вывести в консоль строку "Я люблю JS !" из массива,
+	проходя циклом в обратном порядке, не используя метод reverse.
+	const arr = ['!', 'JS', 'люблю', 'Я'];
+*/
 
-const tasks = ['Задача 1', 'Задача 2', 'Задача 3', 'Задача 4'];
+const arr = ['!', 'JS', 'люблю', 'Я'];
+const arr2 = [];
+const arr3 = [];
 
-// Благодаря continue мы можем пропускать какието элементы
-for (let i = 0; i < tasks.length; i++){
-  if (tasks[i] === 'Задача 2') {
-    continue
-  }
-  console.log(tasks[i]);
+
+// Первый способ - учителя
+for (let i = arr.length - 1; i >= 0; i--) {
+  arr2.push(arr[i])
 }
+console.log(arr2.join(' ')); 
 
-//break прекращает выполнение всего цикла
-for (let i = 0; i < tasks.length; i++){
-  if (tasks[i] === 'Задача 2') {
-    break
-  }
-  console.log(tasks[i]);
+
+// Второй способ - мой
+for(let i = 1; i <= arr.length; i++) {
+  arr3.push(arr[arr.length - i])
 }
+console.log(arr3.join(' '));
+
+//Третий способ - самый простой  ))
+console.log(arr.reverse().join(' '));

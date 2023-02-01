4ac81182e62c36cd395f9f5df0d3e6b98cdc15fd
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-01 22:40:12 +0500

    Continue and break

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-for (let i = 1; i < 10; i++) {
-  console.log(`Наш баланс: ${i}$`);
+//Break and continue
+
+const tasks = ['Задача 1', 'Задача 2', 'Задача 3', 'Задача 4'];
+
+// Благодаря continue мы можем пропускать какието элементы
+for (let i = 0; i < tasks.length; i++){
+  if (tasks[i] === 'Задача 2') {
+    continue
+  }
+  console.log(tasks[i]);
+}
+
+//break прекращает выполнение всего цикла
+for (let i = 0; i < tasks.length; i++){
+  if (tasks[i] === 'Задача 2') {
+    break
+  }
+  console.log(tasks[i]);
 }

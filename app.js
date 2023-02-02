3def2c7cb6a7c1c35e3b0081299f5da178bbf363
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-02 21:28:50 +0500

    for in / for of

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
-//Цикл в while
+//Цикл for of / for in
 
-for (let i = 1; i < 5; i++) {
-	console.log(`Вывод - ${i}`);
-}
-
-//Перепишим на while
-
-let i = 1;
-while (i < 5) {
-	console.log(`Вывод - ${i}`);
-	i++;
-}
-
-//
 const arr = [1, 4, 8, 7];
 
-for (let j = 0; j < arr.length; j++) {
-	if (arr[j] > 5) {
-		break
-	}
-	console.log(arr[j]);
+for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
 }
 
-//Перепишем на while
+//for of - итерируется по ЗНАЧЕНИЯМ элементов массива
 
-let j = 0;
-while (arr[j] <= 5 && j < arr.length) {
-	console.log(arr[j]);
-	j++
+
+
+for (let element of arr) {
+	console.log(element);
 }
 
-//Цикл do while
+//for in - перебор массива по ИНДЕКСУ
 
-let k = 0;
-do {
-	console.log(arr[k]);
-	k++;
-} while (arr[k] <= 5 && k < arr.length)
+for (let index in arr) {
+	console.log(index);
+}
+

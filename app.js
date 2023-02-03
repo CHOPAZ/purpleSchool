0aa5f590486fe6f290d504b3cf4009d871d05772
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-03 22:46:25 +0500

    reduce

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const operations = [100, -20, 7, -20, 50];
+//reduce
 
-// Вывод положительных операций через for
-// const positiveOperations = [];
-// for (const el of operations) {
-//   if (el > 0) {
-//     positiveOperations.push(el)
-//   }
-// }
-// console.log(positiveOperations);
+const operations = [100, -20, 7, -30, 50];
 
-//filter - возвращает массив с элементами, удовлетворяющих условию
+//Итоговая сумма через for
+let balance = 0;
+for (const el of operations) {
+  balance += el;
+}
+console.log(balance);
 
-const positiveOperations = operations.filter(el => {
-  return el > 0;
-});
-console.log(positiveOperations);
+//через reduce
+//prev - аккумулятор, то значение куда будет аккумулироваться все операции
+//value - текущий элемент
+// вконце 0, текущее значение prev
+const finalBalance = operations.reduce((prev, value, i) => {
+  console.log(`Итерация ${i}, prev: ${prev}, value: ${value}`);
+  return prev += value
+}, 0)
+console.log(finalBalance);
 
-//Чейнинг методов
+// Итерация 0, prev: 0, value: 100
+// Итерация 1, prev: 100, value: -20
+// Итерация 2, prev: 80, value: 7
+// Итерация 3, prev: 87, value: -30
+// Итерация 4, prev: 57, value: 50
 
-const positiveRUBOperations = operations
-  .filter(el => {
-    return el > 0;
-  })
-  .map(el => el * 60);
-console.log(positiveRUBOperations);
+//Если мы не укажем в конце текущее значение prev, то он возьмет первый элемент массива
 
+//Реализация минимального элемента
+const minEl = operations.reduce((prev, value) => {
+  if ( value > prev) {
+    return prev
+  } else {
+    return value
+  }
+}, 0)
+console.log(minEl); // -30

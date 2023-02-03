907aea510a162fded534716ebde4d2e79d67dfa7
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-03 21:46:40 +0500

    forEach

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-// function power(pow) {
-// 	return function(num) {
-// 		return num ** pow
-// 	}
+const score = [5, 10, 0, 15];
+
+//for
+for (const el of score) {
+  console.log(el);
+}
+
+for (const el in score) {
+  console.log(`Раунд ${el + 1}: ${score[el]}`);
+}
+
+for (const [i, el] of score.entries()) {
+  console.log(`Раунд ${i + 1}: ${el}`);
+}
+
+//forEach
+
+// const iterator = el => {
+//   console.log(`Раунд: ${el}`);
 // }
+// score.forEach(iterator)
 
-//Запишим в виде стрелочной функции
-const power = pow => num => num ** pow;
+score.forEach(el => {
+  console.log(`Раунд: ${el}`);
+})
 
-console.log(power(2)(5));
+score.forEach((el, i) => {
+  console.log(`Раунд ${i + 1}: ${el}`);
+})
+
+
+// У forEach нет break и continue - поэтому его нельзя прервать !!! У forEach есть проблемы с Асинхронностью
+

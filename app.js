ae658109d79ad52343981d5437295a9f409a8e54
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-03 22:12:20 +0500

    filter

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-//map - итерируется по массиву, и возвращает новый массив
-const transactionInUSD = [10, -7, 50, -10, 100];
+const operations = [100, -20, 7, -20, 50];
 
-// Сколько в рублях были транзакции через for
-const transactionInRUB = [];
-for (const el of transactionInUSD) {
-  transactionInRUB.push(el * 60);
-}
-console.log(transactionInRUB); //[600, -420, 3000, -600, 6000]
-console.log(transactionInUSD); //[10, -7, 50, -10, 100]
+// Вывод положительных операций через for
+// const positiveOperations = [];
+// for (const el of operations) {
+//   if (el > 0) {
+//     positiveOperations.push(el)
+//   }
+// }
+// console.log(positiveOperations);
 
+//filter - возвращает массив с элементами, удовлетворяющих условию
 
-//через map
-
-const transactionInRUB2 = transactionInUSD.map((el) => {
-  return el * 60
+const positiveOperations = operations.filter(el => {
+  return el > 0;
 });
-// const transactionInRUB2 = transactionInUSD.
-//  map((el) => el * 60);
+console.log(positiveOperations);
 
-console.log(transactionInRUB2); //[600, -420, 3000, -600, 6000]
-console.log(transactionInUSD); //[10, -7, 50, -10, 100]
+//Чейнинг методов
+
+const positiveRUBOperations = operations
+  .filter(el => {
+    return el > 0;
+  })
+  .map(el => el * 60);
+console.log(positiveRUBOperations);
+

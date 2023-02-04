d740ced6e242945371ea1948869a11f375d2668d
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-04 21:54:08 +0500

    sort

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
+//sort - сортировка. Меняет исходный массив
 
-//flat - делает плоский массив
-const prices = [[2, 4], [3, 4], [10, 20]];
-const res = prices.flat(); //[2, 4, 3, 4, 10, 20]
-console.log(res);
+const users = ['Vasya', 'Masha', 'Olya', 'Pasha'];
+console.log(users);
 
-const prices2 = [[2, 4], [3, 4], [10, [20, 30]]];
-const res2 = prices2.flat();//[2, 4, 3, 4, 10, [20, 30]]
-console.log(res2); 
-const res3 = prices2.flat(2) //[2, 4, 3, 4, 10, 20, 30]
-console.log(res3);
+// отсортировать по алфавитук
+users.sort(); // ['Masha', 'Olya', 'Pasha', 'Vasya']
+console.log(users);
 
-// flatMap - применение сначала map потом flat
-const prices3 = [[2, 4], [3, 4], [10, [20, 30]]];
-const res4 = prices3.flatMap(el => el.concat([1])) //[2, 4, 1, 3, 4, 1, 10, [20, 30], 1]
-console.log(res4);
+const operations = [100, -20, 30, 480, -200];
+console.log(operations);
+operations.sort(); //[-20, -200, 100, 30, 480] - отсортировалить как строки
+console.log(operations);
+
+const operations2 = [100, -20, 30, 480, -200];
+
+//sort принимает callback, которая содержит два параменра a - 1 элемент, b - 2 элемент, эта ункция должна вернуть либо + либо - значение, в зависимости от того что она вернет, будет порядок либо сохранен либо изменен
+/*
+  Про порядок:
+  < 0 - a, b - сохраняем порядок
+  > 0 - b, a - меняем порядок
+*/
+// длинная запись сортировка по возрастанию
+operations2.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  }
+  if (a < b) {
+    return -1;
+  }
+})
+console.log(operations2);
+
+//  длинная запись сортировка по убыванию
+const operations3 = [100, -20, 30, 480, -200];
+operations3.sort((a, b) => {
+  if (a > b) {
+    return -1;
+  }
+  if (a < b) {
+    return 1;
+  }
+})
+
+console.log(operations3);
+
+// короткая записиь сортировки по убыванию
+const operations4 = [100, -20, 30, 480, -200];
+operations4.sort((a,b) => a - b);
+console.log(operations4);

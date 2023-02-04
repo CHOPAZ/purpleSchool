01167b6a48efa2ffd82d8ee4603a178b2eeb3d1a
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-04 21:37:40 +0500

    flat flatMap

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-/*
-    Напичсать функцию, которая возвращает true, если элемент есть, и false, если нет.
-*/
 
-const arr = [2, 4, 4, 10, 20];
+//flat - делает плоский массив
+const prices = [[2, 4], [3, 4], [10, 20]];
+const res = prices.flat(); //[2, 4, 3, 4, 10, 20]
+console.log(res);
 
-function some (array, element) {
-  const res = array.find(el => el === element)
-  return res === undefined ? false : true
+const prices2 = [[2, 4], [3, 4], [10, [20, 30]]];
+const res2 = prices2.flat();//[2, 4, 3, 4, 10, [20, 30]]
+console.log(res2); 
+const res3 = prices2.flat(2) //[2, 4, 3, 4, 10, 20, 30]
+console.log(res3);
 
-}
-
-console.log(some(arr, 1));
-
-// some - проверяет на наличие элемента
-
-console.log(arr.some(el => el === 2)); 
+// flatMap - применение сначала map потом flat
+const prices3 = [[2, 4], [3, 4], [10, [20, 30]]];
+const res4 = prices3.flatMap(el => el.concat([1])) //[2, 4, 1, 3, 4, 1, 10, [20, 30], 1]
+console.log(res4);

3cf4381ebc0cd84568e200bcf3c4c9127716df10
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-04 20:57:59 +0500

    find and findIndex

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-/* 
-  Найти среднее значение последовательности чисел с помощью reduce
-*/
+const arr = [2, 4, 4, 10, 20];
 
-const arr = [1, 4, 4, 10];
+// find - ищет конкретный элемент
 
-const avg = arr.reduce((prev, value, i) => {
-  if (i != arr.length - 1) {
-    return prev + value
-  } else {
-    return (prev + value) / arr.length
+// вот так работает find
+let elGT5;
+for (const el of arr) {
+  if (el > 5) {
+    elGT5 = el;
+    break;
   }
-}, 0)
-console.log(avg);
+}
+
+console.log(elGT5);
+
+elGT5 = arr.find(el => el > 5); // 10
+console.log(elGT5);
+
+//findeIndex - ищет индекс элемента удовлетворяющий условию
+
+elGT5Index = arr.findIndex(el => el > 5) // 3
+elGT5Index = arr.findIndex(el => el < 0) // -1

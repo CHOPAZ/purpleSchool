ee71da9666ce153d8455f2b8892bf31cb2f5a169
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-05 20:12:06 +0500

    Practice - number check

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
-const str = 'Вася Пупкина';
+// Задание: проверить является ли это номером телефона России
+/* верные */
+const num1 = '89103235356'; // 11
+const num2 = '+79103235356'; // 12
+const num3 = '+7(910)3235356'; //14
+const num4 = '+7(910) 323-53-56'; // 17
+const num5 = ' +7(910) 323-53-56 '; // 19
+/* не верные */
+const num1Error = '89103235'; // 8
+const num2Error = '+7d910d323-53-56';//16
+const num3Error = '9+7103235356'; // 12
+const num4Error = '89103g35356'; // 11
 
-console.log(str.includes('а')); //true
-console.log(str.startsWith('В')); // true
-console.log(str.endsWith('н')); //true
+function isNumber (num) {
+  num = num.trim()
+  num = num.replace('+7', '8');
+  if(!num.startsWith('8')) {
+    return false
+  }
+  num = num.replaceAll('(', '');
+  num = num.replaceAll(')', '');
+  num = num.replaceAll('-', '');
+  num = num.replaceAll(' ', '');
+  if(num.length != 11) {
+    return false
+  }
+  let onlyNum = true;
+  for (const el of num) {
+    if (isNaN(el)) {
+      onlyNum = false;
+      break;
+    }
+  }
+  return onlyNum;
+}
 
-// под капотом это выгледит так, new String - это объект, у которого есть свои методы
-console.log(new String('Вася Пупкин').includes('a'));
+console.log(isNumber(num1));
+console.log(isNumber(num2));
+console.log(isNumber(num3));
+console.log(isNumber(num4));
+console.log(isNumber(num5));
 
+console.log(isNumber(num1Error));
+console.log(isNumber(num2Error));
+console.log(isNumber(num3Error));
+console.log(isNumber(num4Error));
 
-// Преобразование строки, не модифицируют исходную строку
+console.log(isNaN(1234));//false
+console.log(isNaN('1234'));//false
+console.log(isNaN('g'));//true
 
-//простейшие преобразования
-console.log(str.toLowerCase());//вася пупкина
-console.log(str.toUpperCase());//ВАСЯ ПУПКИНА
-console.log(str.replace('В', 'Ф'));//Фася Пупкина
-console.log(str.replaceAll('а', 'и'));//Вися Пупкини
-console.log(str.replace('/а/g', 'и'));//Вися Пупкини
-
-const str2 = ' Вася Пупкина   \n';
-
-//Если хотим очистить строку от пробелов вначале и конце
-console.log(str2.trim()); //Вася Пупкина
-console.log(str2.trimEnd());// _Вася Пупкина
-console.log(str2.trimStart());//Вася Пупкина_
+console.log(Number.isNaN(123));//false
+console.log(Number.isNaN('g'));
+console.log(Number.isNaN('asfsaf'));
 
 
 

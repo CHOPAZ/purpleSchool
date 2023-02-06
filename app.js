31e2fef53f6ee1963bd4645e7d9b2263a85c928f
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-06 20:35:17 +0500

    Practice - sort users by age

--- app.js
+++ app.js
@@ -1,45 +1,14 @@
-// Обращение к элементам
-
-const user = {
-  name: 'Vasya',
-  sername: 'Pupkin',
-  age: 24,
-  skills: [
-    'Programming',
-    'Cooking'
-  ],
-  eduBasic: 'Школа 10',
-  eduPro: 'МФТИ'
-};
-
-//Для рбращения к элементам исп. Member Acsess Notaition
-
-console.log(user); //{name: 'Vasya', sername: 'Pupkin', age: 24, skills: Array(2)}
-console.log(user.name); // Vasya
-console.log(user.skills[0]); //Programming
-
-//Обращение через Breaket Notation
-console.log(user['skills']);
-
 /*
-Разница в обрашении между user.skills и user['skills'] заключается в том что user['skills'] доступно для расчетныйх свойст, а user.skills - нет.
-Что имеется в ввиду?
-
-Допустим у пользователя несколько образованией eduBasic и eduPro и мы хотим обратится к элементу нашего пользователя в зависимости от того что мы запросили Basic or Pro 
+  отсортировать пользователей по возрасту
 */
-const level = 'Pro'
-console.log(user['edu' + level]); // МФТИ
-// console.log(user.'edu' + level); // Ошибка
 
-// const res = prompt('Введите свойство');
-// console.log(user[res]); //age - 24
+const users = [
+  {name: 'Вася', age: 30},
+  {name: 'Катя', age: 18},
+  {name: 'Аня', age: 40},
+  {name: 'Петя', age: 25}
+]
 
-//Добавляем свойства
-user.city = 'Moscow';
-user['city'] = 'Moscow';
-console.log(user);
-
-// Модификация существующих
-
-user.age = 30;
-user['age'] = 30
+console.log(users.sort((a,b) => {
+  return a.age - b.age
+}));

f0a1b59c67a235ec12e30ac9b0b94f65c9b16ff6
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-07 22:45:44 +0500

    Practice - Storage

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
-//Optional chaining - доступ к свойствам если их нет 
-
-const cities = {
-  msk: {
-    temp: {
-      celcius: 25
-    }
-  },
-  spb: {
-
-  }
-}
-console.log(cities.msk.temp.celcius); // 25
-
-// P.s. - попытка взять свойство объекта у undefined вызовет ошибку
-
-// console.log(cities.spb.temp.celcius); // ошибка, tmp - undefined
-
-//Как правильно обратится - неудобно
-if(cities.spb  && cities.spb.temp) {
-  console.log(cities.spb.temp.celcius);
-}
-/* 
-if(cities.spb != undefined  && cities.spb.temp != undefined) {
-  console.log(cities.spb.temp.celcius);
-}
+/*
+	Сделать объект склад с методами добавления на склад, поиска
+	по складку товара и расчёт веса
 */
 
-let city = 'msk';
-if(cities[city]  && cities[city].temp) {
-  console.log(cities[city].temp.celcius);
+const warehouse = {
+  goods: [],
+  findGoodById: function (id) {
+   return  existedGood = this.goods.find(g => g.id === id);
+
+  },
+  addGood: function (good) {
+    const existedGood = this.findGoodById(good.id)
+    if(existedGood) {
+      console.log('Этот товар уже есть на складе');
+      return
+    }
+    this.goods.push(good)
+  },
+  getWeigthKg: function () {
+    return this.goods.reduce((prev, el) => 
+    prev += el.weight?.kg ? el.weight.kg : 0, 0)
+  }
+};
+
+/* товары */
+
+const car = {
+  id: 1,
+  name: 'Car',
+  weight: {
+    kg: 1000
+  },
+  brand: 'Ford'
+};
+
+const chair = {
+  id: 2,
+  name: 'Chair',
+  weight: {
+    kg: 2
+  }
 }
 
-//Как правильно обратится - удобно и правильно через Optional chaining - ?.
-console.log(cities[city]?.temp?.celcius);
+const paper = {
+  id: 3,
+  name: 'Paper',
+  color: 'red'
+}
+
+warehouse.addGood(car);
+warehouse.addGood(car); //Этот товар уже есть на складе
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse.goods);
+let findeItems = warehouse.findGoodById(2);
+console.log(findeItems);
+const w = warehouse.getWeigthKg();
+console.log(w);

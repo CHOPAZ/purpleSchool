929962615b3d6db2c9a0780c800675c5685489e8
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-07 21:24:45 +0500

    Practice - Wallet

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-// Методы объектов - this - контекст (про него в отдельном модуле 15, 16)
+/* Реализовать методы увеличения и уменьшения баланса,
+при котором каждая операция сохраняется в массив
+operations в виде { reason: 'Оплата налогов', sum: -100 }.
+Возвращается true, если успешно и false, если не зватает баланса.
+Также реализовать метод вывода числа операций по кошельку
+*/
 
-const user = {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 24,
-    getFullName: function() {
-      return this.name + ' ' + this.surname
+const wallet = {
+  balance: 0,
+  operations: [],
+  getIncreaseBalance: function (reasons, sums) {
+      this.balance += sums;
+      return this.operations.push({reason: reasons, sum: sums})
+  },
+  getDecreaseBalance: function (reasons, sums) {
+    if (this.balance < sums) {
+      console.log('Недостаточно баланса');
+      return false;
     }
-}
+      this.balance -= sums;
+      this.operations.push({reason: reasons, sum: -sums});
+      return true
+  },
+  getAmountOperations: function () {
+    return this.operations.length
+  }
+};
 
-console.log(user.getFullName());
+console.log(wallet);
+wallet.getIncreaseBalance('Чаевые', 10000);
+wallet.getDecreaseBalance('Оплата налогов', 1000);
+console.log(wallet.getAmountOperations());

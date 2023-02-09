ff77e3bf3756add77dea90ccc9668435eef901fc
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-09 20:43:12 +0500

    Example work call stack

--- app.js
+++ app.js
@@ -1,59 +1,14 @@
-/*
-	Сделать объект склад с методами добавления на склад, поиска
-	по складку товара и расчёт веса
-*/
+const sum = 1;
+console.log(sum);
 
-const warehouse = {
-  goods: [],
-  findGoodById: function (id) {
-   return  existedGood = this.goods.find(g => g.id === id);
-
-  },
-  addGood: function (good) {
-    const existedGood = this.findGoodById(good.id)
-    if(existedGood) {
-      console.log('Этот товар уже есть на складе');
-      return
-    }
-    this.goods.push(good)
-  },
-  getWeigthKg: function () {
-    return this.goods.reduce((prev, el) => 
-    prev += el.weight?.kg ? el.weight.kg : 0, 0)
+function add5(n) {
+  n = n + 5;
+  if (n > 10) {
+    return n
   }
-};
-
-/* товары */
-
-const car = {
-  id: 1,
-  name: 'Car',
-  weight: {
-    kg: 1000
-  },
-  brand: 'Ford'
-};
-
-const chair = {
-  id: 2,
-  name: 'Chair',
-  weight: {
-    kg: 2
-  }
+  console.log(n);
+  return add5(n)
 }
 
-const paper = {
-  id: 3,
-  name: 'Paper',
-  color: 'red'
-}
-
-warehouse.addGood(car);
-warehouse.addGood(car); //Этот товар уже есть на складе
-warehouse.addGood(chair);
-warehouse.addGood(paper);
-console.log(warehouse.goods);
-let findeItems = warehouse.findGoodById(2);
-console.log(findeItems);
-const w = warehouse.getWeigthKg();
-console.log(w);
+add5(sum);
+console.log('ended');

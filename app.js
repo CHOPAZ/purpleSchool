cc97b34bc5f6d3f0b28fc70bc69868477e90f572
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-13 21:39:13 +0500

    Example user this

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-function addUser() {
-	// console.log('User added');
+/*
+  Что такое this?
+
+  Переменная, которая создается для каждого контекста исполнения. Она указывает на владельца этой функции
+
+  this не статичен и определяется только при вызове функции в зависимости от того, как она вызвана
+*/
+
+'use strict';
+
+console.log(this); //window
+console.log(window); //window
+
+function addNum(num1, num2) {
+  console.log(this); // undefined - при use strict, без него ссылкается на window
+  return num1 + num2
 }
 
-let arr1 = () => {
-	console.log('arr1');
+const  addNum2 = (num1, num2) => {
+  console.log(this); // window - при use strict
+  return num1 + num2
 }
 
-addUser();
-console.log(b);
-let a = 3;
-var b = 2;
-console.log(b);
+addNum();
+addNum2();
 
+const user = {
+  name: 'Vasya',
+  surname: 'Pupkin',
+  getFullName: function() {
+    console.log(this);
+    return this.name + ' ' + this.surname
+  }
+}
+user.getFullName(); // this - user
 
-arr1();
+const user2 = {
+  name: 'Petya',
+  surname: 'ASF'
+}
 
+user2.getFullName = user.getFullName;
+user2.getFullName(); // this - user2
 
+const getFullName = user2.getFullName;
 
-addUser();
+getFullName() // this - undefined и ошибка

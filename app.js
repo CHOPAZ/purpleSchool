ebc751452a3fb7a62d7b82ff6daff0b0b1deb6bb
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-13 21:52:40 +0500

    Context in methods

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
-/*
-  Что такое this?
-
-  Переменная, которая создается для каждого контекста исполнения. Она указывает на владельца этой функции
-
-  this не статичен и определяется только при вызове функции в зависимости от того, как она вызвана
-*/
+// Контекст в методах
 
 'use strict';
 
-console.log(this); //window
-console.log(window); //window
-
-function addNum(num1, num2) {
-  console.log(this); // undefined - при use strict, без него ссылкается на window
-  return num1 + num2
-}
-
-const  addNum2 = (num1, num2) => {
-  console.log(this); // window - при use strict
-  return num1 + num2
-}
-
-addNum();
-addNum2();
-
 const user = {
-  name: 'Vasya',
-  surname: 'Pupkin',
-  getFullName: function() {
+  firstName: 'Vasya',
+  lastName: 'Pupkin',
+  age: 20,
+  getUserInfo: function() {
     console.log(this);
-    return this.name + ' ' + this.surname
-  }
+    console.log(`${this.firstName} ${this.lastName}`);
+
+    function canDrink() {
+      if(this.age >= 18) {
+        console.log('Может уже пить!');
+      } else {
+        console.log('Не может уже пить!');
+      }
+    }
+    canDrink();// будет ошибка так как this undefined, для решения использовать стрелочную функцию
+  },
+  getUserInfoArrow: () => {
+    console.log(this);//window
+    console.log(`${this.firstName} ${this.lastName}`);
+  }  
 }
-user.getFullName(); // this - user
 
-const user2 = {
-  name: 'Petya',
-  surname: 'ASF'
-}
-
-user2.getFullName = user.getFullName;
-user2.getFullName(); // this - user2
-
-const getFullName = user2.getFullName;
-
-getFullName() // this - undefined и ошибка
+user.getUserInfo();//Vasya Pupkin
+user.getUserInfoArrow();//undefined undefined

55b43b7c0229a40b6c82c47b830122393149271a
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-02-14 20:20:23 +0500

    Call, apply

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-//EOL - enhanced object literal для методов
+//Call, apply 
 
+'use strict';
 
-'use strict'
-
-const b = 1;
-
-const a = {
-  b,
-  getB: function () {
-    return this.b;
-  },
-  getBAlt () { // EOL
-    return this.b;
+const audi = {
+  make : 'Audi',
+  model: 'A3',
+  year: 2021,
+  damages: [],
+  addDamage (part, rate) {
+    console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено: - повреждение ${part} со степенью ${rate}`);
+    this.damages.push({
+      part,
+      rate
+    })
   }
 }
 
-console.log(a.getBAlt());
+audi.addDamage('Капот', 1);
+
+const bmw = {
+  make : 'BMW',
+  model: 'X5',
+  year: 2022,
+  damages: []
+};
+
+bmw.addDamage = audi.addDamage;
+bmw.addDamage('Бампер', 2);
+
+
+const addDamageFunc = audi.addDamage;
+// addDamageFunc('Бампер', 2); // undefined - потомучто это обычная функция, не объект
+
+//Call - вызови функцию а дальше на каком контексте и с какими аргументами
+
+addDamageFunc.call(bmw, 'Бампер', 2);
+addDamageFunc.call(audi, 'Бампер', 2);
+
+//Apply - приминить метод на каком то объекте, аргументы записываются в массив
+
+addDamageFunc.apply(bmw, ['Бампер', 2])
+
